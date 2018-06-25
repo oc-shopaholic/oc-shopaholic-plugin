46df26b9abce3ba7c6dd1314119c636cc83a4c0e
Andrey Kharanenka <a.khoronenko@lovata.com>
2018-06-25 09:36:59 +0300

    Adding getPageUrl() method to ProductItem, BrandItem classes.

--- classes/item/BrandItem.php
+++ classes/item/BrandItem.php
@@ -1,6 +1,9 @@
 <?php namespace Lovata\Shopaholic\Classes\Item;
 
+use Cms\Classes\Page as CmsPage;
+
 use Lovata\Toolbox\Classes\Item\ElementItem;
+use Lovata\Toolbox\Classes\Helper\PageHelper;
 
 use Lovata\Shopaholic\Models\Brand;
 
@@ -29,4 +32,41 @@ class BrandItem extends ElementItem
 
     /** @var Brand */
     protected $obElement = null;
+
+    /**
+     * Returns URL of a category page.
+     *
+     * @param string $sPageCode
+     *
+     * @return string
+     */
+    public function getPageUrl($sPageCode = 'brand')
+    {
+        //Get URL params
+        $arParamList = $this->getPageParamList($sPageCode);
+
+        //Generate page URL
+        $sURL = CmsPage::url($sPageCode, $arParamList);
+
+        return $sURL;
+    }
+
+    /**
+     * Get URL param list by page code
+     * @param string $sPageCode
+     * @return array
+     */
+    public function getPageParamList($sPageCode) : array
+    {
+        $arPageParamList = [];
+
+        //Get URL params for page
+        $arParamList = PageHelper::instance()->getUrlParamList($sPageCode, 'BrandPage');
+        if (!empty($arParamList)) {
+            $sPageParam = array_shift($arParamList);
+            $arPageParamList[$sPageParam] = $this->slug;
+        }
+
+        return $arPageParamList;
+    }
 }
