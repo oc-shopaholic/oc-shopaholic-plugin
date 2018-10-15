a79f2776f9d92d2a55d0f62a1b6b910bb53fac84
Pavel Lovtsevich <p.lovtsevich@lovata.com>
2018-10-15 14:41:23 +0300

    New translations lang.php (Russian)

--- lang/ru/lang.php
+++ lang/ru/lang.php
@@ -1,9 +1,9 @@
 <?php return [
-    'plugin'     => [
+    'plugin'      => [
         'name'        => 'Шопоголик',
         'description' => 'Каталог продукции для электронной коммерции',
     ],
-    'field' => [
+    'field'       => [
         'vendor_code'         => 'Артикул',
         'price'               => 'Цена',
         'old_price'           => 'Старая цена',
@@ -13,8 +13,10 @@
         'currency'            => 'Валюта',
         'check_offer_active'  => 'При получении списка активных товаров проверять наличие активных предложений',
         'additional_category' => 'Additional categories',
+        'promo_block_type'    => 'Promo block with product list',
+        'promo_block'         => 'Promo block',
     ],
-    'menu'       => [
+    'menu'        => [
         'main'                => 'Каталог товаров',
         'categories'          => 'Категории товаров',
         'product'             => 'Товары',
@@ -22,71 +24,91 @@
         'shop_catalog'        => 'Каталог продукции',
         'shop_category'       => 'Каталог продукции',
         'all_shop_categories' => 'Все категории продуктов',
+        'promo_block'         => 'Promo blocks',
+        'promo'               => 'Promo',
     ],
-    'tab'        => [
-        'offer'         => 'Товарные предложения',
-        'permissions'   => 'Шопоголик',
+    'tab'         => [
+        'offer'       => 'Товарные предложения',
+        'price'       => 'Prices',
+        'permissions' => 'Шопоголик',
     ],
-    'category'   => [
+    'category'    => [
         'name'       => 'категории',
         'list_title' => 'Список категорий',
     ],
-    'brand'      => [
+    'brand'       => [
         'name'       => 'бренда',
         'list_title' => 'Список брендов',
     ],
-    'product'    => [
+    'product'     => [
         'name'       => 'товара',
         'list_title' => 'Список товаров',
     ],
-    'offer'      => [
+    'offer'       => [
         'name'       => 'предложения',
         'list_title' => 'Список предложений',
     ],
-    'component'  => [
+    'promo_block' => [
+        'name'       => 'promo block',
+        'list_title' => 'Promo block list',
+    ],
+    'component'   => [
 
         //Product components
-        'product_page_name'         => 'Страница товара',
-        'product_page_description'  => 'Получение данных для страницы товара',
-        'product_data_name'         => 'Данные товара',
-        'product_data_description'  => 'Получение данных товара',
-        'product_list_name'         => 'Список товаров',
-        'product_list_description'  => 'Показать список продуктов',
+        'product_page_name'            => 'Страница товара',
+        'product_page_description'     => 'Получение данных для страницы товара',
+        'product_data_name'            => 'Данные товара',
+        'product_data_description'     => 'Получение данных товара',
+        'product_list_name'            => 'Список товаров',
+        'product_list_description'     => 'Показать список продуктов',
 
         //Brand components
-        'brand_page_name'           => 'Страница бренда',
-        'brand_page_description'    => 'Получение данных для страницы бренда',
-        'brand_data_name'           => 'Данные бренда',
-        'brand_data_description'    => 'Получение данных бренда',
-        'brand_list_name'           => 'Список брендов',
-        'brand_list_description'    => 'Получение списка брендов',
+        'brand_page_name'              => 'Страница бренда',
+        'brand_page_description'       => 'Получение данных для страницы бренда',
+        'brand_data_name'              => 'Данные бренда',
+        'brand_data_description'       => 'Получение данных бренда',
+        'brand_list_name'              => 'Список брендов',
+        'brand_list_description'       => 'Получение списка брендов',
+
+        //Promo block components
+        'promo_block_page_name'        => 'Promo block page',
+        'promo_block_page_description' => 'Get data for promo block page',
+        'promo_block_data_name'        => 'Promo block data',
+        'promo_block_data_description' => 'Get promo block data by ID',
+        'promo_block_list_name'        => 'Promo block list',
+        'promo_block_list_description' => 'Get promo block list',
 
         //Category components
-        'category_page_name'        => 'Страница категории',
-        'category_page_description' => 'Вывод данных на странице категории',
-        'category_data_name'        => 'Данные категории',
-        'category_data_description' => 'Получение данных категории',
-        'category_list_name'        => 'Список категорий',
-        'category_list_description' => 'Получение дерева категорий',
+        'category_page_name'           => 'Страница категории',
+        'category_page_description'    => 'Вывод данных на странице категории',
+        'category_data_name'           => 'Данные категории',
+        'category_data_description'    => 'Получение данных категории',
+        'category_list_name'           => 'Список категорий',
+        'category_list_description'    => 'Получение дерева категорий',
 
         //Common components
-        'breadcrumbs_name'          => 'Хлебные крошки',
-        'breadcrumbs_description'   => 'Получение данных для формирования хлебных крошек каталога',
+        'breadcrumbs_name'             => 'Хлебные крошки',
+        'breadcrumbs_description'      => 'Получение данных для формирования хлебных крошек каталога',
 
         //Components settings
-        'product_list_sorting'      => 'Сортировка по умолчанию',
-        'sorting_no'                => 'Без сортировки',
-        'sorting_price_desc'        => 'Дорогие',
-        'sorting_price_asc'         => 'Дешевые',
-        'sorting_new'               => 'Новые',
-        'sorting_popularity_desc'   => 'Популярные',
-        'sorting_rating_desc'       => 'Высокий рейтинг',
-        'sorting_rating_asc'        => 'Низкий рейтинг',
+        'product_list_sorting'         => 'Сортировка по умолчанию',
+        'sorting_no'                   => 'Без сортировки',
+        'sorting_price_desc'           => 'Дорогие',
+        'sorting_price_asc'            => 'Дешевые',
+        'sorting_new'                  => 'Новые',
+        'sorting_popularity_desc'      => 'Популярные',
+        'sorting_rating_desc'          => 'Высокий рейтинг',
+        'sorting_rating_asc'           => 'Низкий рейтинг',
+        'sorting_date_begin_asc'       => 'Date begin (ASC)',
+        'sorting_date_begin_desc'      => 'Date begin (DESC)',
+        'sorting_date_end_asc'         => 'Date end (ASC)',
+        'sorting_date_end_desc'        => 'Date end (DESC)',
     ],
-    'permission' => [
-        'category' => 'Управление категориями',
-        'brand'    => 'Управление брендами',
-        'product'  => 'Управление товарами',
-        'settings' => 'Управление настройками',
+    'permission'  => [
+        'category'    => 'Управление категориями',
+        'brand'       => 'Управление брендами',
+        'product'     => 'Управление товарами',
+        'settings'    => 'Управление настройками',
+        'promo_block' => 'Manage promo blocks',
     ],
 ];
